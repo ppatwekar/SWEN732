8ecf6bd544a828e5bb7ddf797687ed1ee805d209
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-04-04 15:37:50 -0400

    Added a script that creates users and passwords tables and inserts some initial data into it so all developers can use this data to start developing features

---
+++ src/sample/dao/initialScripts/SeedData.java
@@ -0,0 +1,127 @@
+package sample.dao.initialScripts;
+
+import java.sql.*;
+
+public class SeedData {
+    public static void main(String[] args) throws SQLException {
+        String database_username = args[0];
+        String database_password = args[1];
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/password_manager",database_username,database_password);
+
+        script(connection);
+    }
+
+    public static void script(Connection connection){
+        createUsersTable(connection);
+        createPasswordsTable(connection);
+        addSeedDataUsersTable(connection);
+        addSeedDataPasswordsTable(connection);
+    }
+
+    public static void createUsersTable(Connection connection) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+
+            String createTableStatement = "CREATE TABLE users " +
+                    "(id INTEGER NOT NULL AUTO_INCREMENT, " +
+                    "email VARCHAR(255) NOT NULL, " +
+                    "master_password VARCHAR(255) NOT NULL," +
+                    "PRIMARY KEY (id))";
+
+            statement.executeUpdate(createTableStatement);
+            System.out.println("Created table users in given database");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addSeedDataUsersTable(Connection connection){
+        try {
+            Statement statement = connection.createStatement();
+
+            String insertData = "INSERT INTO users(email, master_password) " +
+                    "VALUES (\"p@p.com\", \"passwordify\"), " +
+                    "(\"check@mate.com\", \"matecheck\"), " +
+                    "(\"swen@732.com\", \"golisano\");";
+
+            statement.executeUpdate(insertData);
+            System.out.println("Initial data to user table added");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void createPasswordsTable(Connection connection){
+        try {
+            Statement s = connection.createStatement();
+
+            String createPasswordsTableStatement = "CREATE TABLE passwords " +
+                    "(id INTEGER NOT NULL AUTO_INCREMENT, " +
+                    "user_id INTEGER NOT NULL, " +
+                    "label VARCHAR(255) NOT NULL, " +
+                    "username VARCHAR(255), " +
+                    "password VARCHAR(255) NOT NULL, " +
+                    "notes VARCHAR(255), " +
+                    "PRIMARY KEY (id), " +
+                    "FOREIGN KEY (user_id) REFERENCES users(id));";
+
+            s.executeUpdate(createPasswordsTableStatement);
+            System.out.println("Created passwords table in database");
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void addSeedDataPasswordsTable(Connection connection){
+        try {
+            String sql = "INSERT INTO passwords(user_id, label, username, password, notes) VALUES(?,?,?,?,?)";
+
+            String[][] data = {
+                    {"2","test label",null,"testpassword","test notes"},
+                    {"1","insta","gram","software",null},
+                    {"3","snap","castle","geology",null},
+                    {"1","beach",null,"waves",null},
+                    {"2","monster","energy","Alfa","romeo"}
+            };
+
+            for(String[] d : data){
+                PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+                preparedStatement.setInt(1,Integer.parseInt(d[0]));
+
+                preparedStatement.setString(2,d[1]);
+
+                insertNullHelper(preparedStatement, 3, d, 2);
+
+                preparedStatement.setString(4,d[3]);
+
+                insertNullHelper(preparedStatement, 5, d,4);
+
+                preparedStatement.executeUpdate();
+            }
+
+
+            System.out.println("Inserted password data");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertNullHelper(PreparedStatement preparedStatement,  int parameterIndex, String[] tuple, int dataIndex) throws SQLException {
+        if(tuple[dataIndex] == null){
+            preparedStatement.setNull(parameterIndex,Types.NULL);
+        }
+        else{
+            preparedStatement.setString(parameterIndex, tuple[dataIndex]);
+
+        }
+    }
+
+}
