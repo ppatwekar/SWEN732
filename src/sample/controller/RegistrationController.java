2acf0fb5f0ba241fa943e96644bcd0abab34fc4a
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2023-04-23 16:18:12 -0400

    Changes to controller and ui

--- src/sample/controller/RegistrationController.java
+++ src/sample/controller/RegistrationController.java
@@ -1,5 +1,29 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 public class RegistrationController {
 
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button closeButtonRegisterScreen;
+
+    @FXML
+    private PasswordField userPasswordTextField;
+
+    @FXML
+    private TextField userEmailTextField;
+
+
+    public void registerButtonOnAction(ActionEvent actionEvent) {
+
+    }
 }
